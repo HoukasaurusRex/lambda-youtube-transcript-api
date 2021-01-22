59060705a974c08580a5f763530c46db5b404c57
HoukasaurusRex <jt@houk.space>
2021-01-22 11:54:03 +0800

    it works!

--- handler.py
+++ handler.py
@@ -1,24 +1,34 @@
 import json
 from youtube_transcript_api import YouTubeTranscriptApi
-from aws_lambda_powertools.utilities import typing, data_classes
+# from aws_lambda_powertools.utilities import typing, data_classes
 
-APIGatewayProxyEvent = data_classes.APIGatewayProxyEvent
-LambdaContext = typing.LambdaContext
+# APIGatewayProxyEvent = data_classes.APIGatewayProxyEvent
+# LambdaContext = typing.LambdaContext
 
-class QueryStringModel(APIGatewayProxyEvent):
-  videoId: str
+# class QueryStringModel(APIGatewayProxyEvent):
+#   videoId: str
 
-def get_youtube_transcript(event: QueryStringModel, context: LambdaContext):
-  video_id = event['queryStringParameters']['videoId']
-  transcript = YouTubeTranscriptApi.get_transcript(video_id)
-  print(transcript)
-  body = {
-    "message": "success",
-    "data": transcript
-  }
-  response = {
-    "statusCode": 200,
-    "body": json.dumps(body)
-  }
-
-  return response
+# def get_youtube_transcript(event: QueryStringModel, context: LambdaContext):
+def get_youtube_transcript(event, context):  
+  try:
+    video_id = event['queryStringParameters']['videoId']
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    statusCode = 200
+    body = {
+      "message": "success",
+      "data": transcript
+    }
+    pass
+  except Exception as error:
+    statusCode = 400
+    body = error
+    pass
+  finally:
+    response = {
+      "statusCode": statusCode,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "body": json.dumps(body)
+    }
+    return response
